dfdbaedb07227484e3798926a4a8818e8b91ad21
Lucas Bigelow <lukejbigelow@gmail.com>
2022-03-08 15:50:01 -0600

    making successful get request, and displaying information on page using Todo and TodoList

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+
+import Form from './Form';
+import TodoList from './TodoList';
 
 const URL = 'http://localhost:9000/api/todos'
 
+const initialState = {
+  todos: [],
+}
+
 export default class App extends React.Component {
+  state = initialState;
+
+  componentDidMount() {
+    axios.get(URL)
+      .then(res => {
+        console.log(res.data.data)
+        this.setState({...this.state, todos: res.data.data})
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
-    return null
+    return (
+      <TodoList todos={this.state.todos} />
+    )
   }
 }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default class Todo extends React.Component {
   render() {
-    return null
+    console.log(this.props.todo.name)
+    return (
+      <h1>{this.props.todo.name}</h1>
+    )
   }
 }
